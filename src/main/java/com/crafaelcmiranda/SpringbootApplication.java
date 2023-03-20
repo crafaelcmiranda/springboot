5a5f42d4af9ade379bfa051c687cf9d473d26a63
crafaelcmiranda <crafaelcmiranda@gmail.com>
2023-03-20 10:25:30 -0300

    Ajustes de pacotes

--- src/main/java/com/crafaelcmiranda/SpringbootApplication.java
+++ src/main/java/com/crafaelcmiranda/SpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.crafaelcmiranda.springboot;
+package com.crafaelcmiranda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/crafaelcmiranda/SpringbootApplicationTests.java
+++ src/test/java/com/crafaelcmiranda/SpringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.crafaelcmiranda.springboot;
+package com.crafaelcmiranda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
